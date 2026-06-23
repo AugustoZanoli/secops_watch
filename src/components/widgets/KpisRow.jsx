18f1582e3ef31755e276dc21fb29d8a5403dc55e
Matheus Anthony <matheus.anthony.pa@outlook.com>
2026-06-22 23:23:20 -0300

    feat: cria placeholders dos 13 widgets

---
+++ src/components/widgets/KpisRow.jsx
@@ -0,0 +1,7 @@
+export function KpisRow() {
+  return (
+    <div className="border border-dashed border-gray-700 rounded-xl p-4 text-center">
+      <p className="text-xs text-gray-500">KpisRow (TODO)</p>
+    </div>
+  )
+}
---
+++ src/components/widgets/TopUsersChart.jsx
@@ -0,0 +1,7 @@
+export function TopUsersChart() {
+  return (
+    <div className="border border-dashed border-gray-700 rounded-xl p-4 text-center">
+      <p className="text-xs text-gray-500">TopUsersChart (TODO)</p>
+    </div>
+  )
+}
